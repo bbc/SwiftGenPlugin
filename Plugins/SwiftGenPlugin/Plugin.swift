37b0dd02a3aa820764c59ae02de1ea99beac5a90
Peter Bloxidge <peter.bloxidge@bbc.co.uk>
2023-09-01 18:03:03 +0100

    Add support for XcodeProjectPlugin

--- Plugins/SwiftGenPlugin/Plugin.swift
+++ Plugins/SwiftGenPlugin/Plugin.swift
@@ -6,14 +6,17 @@
 
 import Foundation
 import PackagePlugin
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+#endif
 
 @main
-struct SwiftGenPlugin: BuildToolPlugin {
-  func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+struct SwiftGenPlugin {
+  func createBuildCommands(context: SharedPluginContext, target: SharedTarget) throws -> [Command] {
     let fileManager = FileManager.default
 
     // Possible paths where there may be a config file (root of package, target dir.)
-    let configurations: [Path] = [context.package.directory, target.directory]
+    let configurations: [Path] = [context.rootDirectory, target.directory]
       .map { $0.appending("swiftgen.yml") }
       .filter { fileManager.fileExists(atPath: $0.string) }
 
@@ -31,11 +34,25 @@ struct SwiftGenPlugin: BuildToolPlugin {
   }
 }
 
+extension SwiftGenPlugin: BuildToolPlugin {
+  func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+    try createBuildCommands(context: context, target: .init(target))
+  }
+}
+
+#if canImport(XcodeProjectPlugin)
+extension SwiftGenPlugin: XcodeBuildToolPlugin {
+  func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+    try createBuildCommands(context: context, target: .init(target, project: context.xcodeProject))
+  }
+}
+#endif
+
 // MARK: - Helpers
 
 private extension SwiftGenPlugin {
   /// Validate the given list of configurations
-  func validate(configurations: [Path], target: Target) -> Bool {
+  func validate(configurations: [Path], target: SharedTarget) -> Bool {
     guard !configurations.isEmpty else {
       Diagnostics.error("""
       No SwiftGen configurations found for target \(target.name). If you would like to generate sources for this \
@@ -50,7 +67,7 @@ private extension SwiftGenPlugin {
 }
 
 private extension Command {
-  static func swiftgen(using configuration: Path, context: PluginContext, target: Target) throws -> Command {
+  static func swiftgen(using configuration: Path, context: SharedPluginContext, target: SharedTarget) throws -> Command {
     .prebuildCommand(
       displayName: "SwiftGen BuildTool Plugin",
       executable: try context.tool(named: "swiftgen").path,
@@ -61,9 +78,9 @@ private extension Command {
         "--config", "\(configuration)"
       ],
       environment: [
-        "PROJECT_DIR": context.package.directory,
+        "PROJECT_DIR": context.rootDirectory,
         "TARGET_NAME": target.name,
-        "PRODUCT_MODULE_NAME": target.moduleName,
+        "PRODUCT_MODULE_NAME": target.productModuleName,
         "DERIVED_SOURCES_DIR": context.pluginWorkDirectory
       ],
       outputFilesDirectory: context.pluginWorkDirectory
@@ -91,3 +108,47 @@ extension Target {
     }
   }
 }
+
+#if canImport(XcodeProjectPlugin)
+extension XcodeTarget {
+  var productName: String {
+    return product?.name ?? ""
+  }
+}
+#endif
+
+struct SharedTarget {
+  let name: String
+  let productModuleName: String
+  let directory: Path
+
+  init(_ target: Target) {
+    self.name = target.name
+    self.productModuleName = target.moduleName
+    self.directory = target.directory
+  }
+
+#if canImport(XcodeProjectPlugin)
+  init(_ target: XcodeTarget, project: XcodeProject) {
+    self.name = target.displayName
+    self.productModuleName = target.productName
+    self.directory = project.directory.appending(subpath: target.displayName)
+  }
+#endif
+}
+
+protocol SharedPluginContext {
+  var rootDirectory: Path { get }
+  var pluginWorkDirectory: Path { get }
+  func tool(named name: String) throws -> PluginContext.Tool
+}
+
+extension PluginContext: SharedPluginContext {
+  var rootDirectory: Path { package.directory }
+}
+
+#if canImport(XcodeProjectPlugin)
+extension XcodePluginContext: SharedPluginContext {
+  var rootDirectory: Path { xcodeProject.directory }
+}
+#endif
